86b70d15b03fc023e5af1706a7a940dcb6d40105
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-03 17:56:51 +0100

    Bug hippo hello doesn't work, hello does #3
    * npm install --save yargs
    
    * Accept hello option argument
    
    * Update package.json

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+const yargs = require("yargs");
+
+const options = yargs
+ .usage("Usage: -h")
+ .option("h", { alias: "hello", describe: "Say hello", type: "string" })
+ .argv;
+
 console.log('hippo says "honk!"');
