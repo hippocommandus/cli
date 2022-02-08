87aadd8ba764b946b6a13589599c9513aaf71dec
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-08 11:48:07 +0100

    Adding MixPanel & setting up events to track

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,34 @@
 
 const yargs = require("yargs");
 
-const options = yargs
- .usage("Usage: -h")
- .option("h", { alias: "hello", describe: "Say hello", type: "string" })
- .argv;
+const Mixpanel = require('mixpanel');
+const mixpanel = Mixpanel.init('2cd8c150d674ae1b223956e7a6a05fd2', {
+    host: "api-eu.mixpanel.com",
+});
 
-console.log('hippo says "honk!"');
+const os = require('os');
+const computerName = os.hostname()
+
+const options = yargs
+    .usage("Usage: -h")
+    .option("h", { alias: "hello", describe: "Say hello", type: "string" })
+    .option("r", { alias: "recall", describe: "Recall your command", type: "string" })
+    .argv;
+
+if (options.hasOwnProperty('hello')) {
+    mixpanel.track('hippo', {
+        distinct_id: computerName,
+        option: '--hello',
+    });
+
+    console.log('hippo says "honk!"');
+} 
+
+if (options.hasOwnProperty('recall')) {
+    mixpanel.track('hippo', {
+        distinct_id: computerName,
+        option: '--recall',
+    });
+
+    console.log("honk! i'm recalling...");
+} 
